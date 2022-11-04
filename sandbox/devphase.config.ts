9d79c1c7ee4621227660708aa61ae9934e1f5aea
l00k <l00k+github@protonmail.com>
2022-11-04 16:55:47 +0100

    stack setpu refactor, exposing new options: logging in testing mode, block time adjustment

--- sandbox/devphase.config.ts
+++ sandbox/devphase.config.ts
@@ -1,5 +1,19 @@
 import type { ProjectConfigOptions } from 'devphase';
+import { DevPhase } from 'devphase/dist';
 
-const config : ProjectConfigOptions = {};
+const config : ProjectConfigOptions = {
+    testing: {
+        blockTime: 250,
+        stackLogOutput: true,
+        envSetup: {
+            setup: {
+                custom: async (devPhase : DevPhase) => {
+                    await devPhase.defaultEnvSetup();
+                    console.log('Using default env setup done');
+                },
+            }
+        }
+    }
+};
 
 export default config;
